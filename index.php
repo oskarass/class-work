e60bfaa30ca6179ea5769a66723b8a5c3daf29bc
Gytis <velniopviska@gmail.com>
2019-12-10 13:35:26 +0200

    updated

--- index.php
+++ index.php
@@ -1,25 +1,64 @@
 <?php
 
+$status = "";
+$displayCert = false;
+
 if (isset($_POST['submit'])) {
-    $num = $_POST['submit'] +1;
-    for($i=1; $i <= $num; $i++) {
-        print "<img src=\"https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?w=200&quality=85\">";
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $age = $_POST['age'];
+    $level = $_POST['level'];
+
+    if (empty($name) || empty($lastname) || empty($age) || empty($level)) {
+        $status = "All fields must be filled!";
+    } else {
+        if (!preg_match("/[a-zA-Z-'\s]+$/", $name) || !preg_match("/[a-zA-Z-'\s]+$/", $lastname)) {
+            $status = "Enter a valid Name and Last name!";
+        } elseif (is_int($age)) {
+            $status = "Only numbers required";
+        } else {
+            $displayCert = true;
+            $name = "Name: $name";
+            $lastname = "Last name: $lastname";
+            $age = "Age: $age";
+            $level = "Level: $level";
+        }
     }
-} else {
-    $num = 0;
+
 }
 
 ?>
 
 <html>
-    <head>
-        <title>Class work</title>
-    </head>
-    <body>
-        <form method="post">
-            <input type="submit" name="submit" value="<?php print $num; ?>">
-        </form>
-    </body>
+<head>
+    <title>Class work</title>
+</head>
+<body>
+    <h4>Dumbass Form</h4>
+    <form method="post">
+        <input type="name" name="name" placeholder="Name" required>
+        <input type="name" name="lastname" placeholder="Last name" required>
+        <input type="number" name="age" placeholder="Age">
+        <select name="level">
+            <option>Pradedantysis</option>
+            <option>Pazenges</option>
+            <option>Profesionalas</option>
+        </select>
+        <input type="submit" name="submit" value="Generate form">
+    </form>
+    <h3><?php print $status; ?></h3>
+    
+    <?php if($displayCert) : ?>
+        <div>
+            <h2>Dumbass Certificate</h2>
+            <h4><?php print $name; ?></h4>
+            <h4><?php print $lastname; ?></h4>
+            <h4><?php print $age; ?></h4>
+            <h4><?php print $level; ?></h4>
+            <img src="http://www.liberaldictionary.com/wp-content/uploads/2019/01/dumbass-3.jpg">
+        </div>
+    <?php endif; ?>
+</body>
 </html>
 
 
