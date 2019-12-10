a5453621de060a109a0b929f252491d201d418f6
Gytis <velniopviska@gmail.com>
2019-12-10 12:34:53 +0200

    updated

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 if (isset($_POST['submit'])) {
     $num = $_POST['submit'] +1;
+    for($i=1; $i <= $num; $i++) {
+        print "<img src=\"https://api.time.com/wp-content/uploads/2019/11/gettyimages-459761948.jpg?w=200&quality=85\">";
+    }
 } else {
     $num = 0;
 }
