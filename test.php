86276ea99b120df10cbfd3f776fec57e9c1c9cdb
Gytis <velniopviska@gmail.com>
2020-01-03 11:27:07 +0200

    classwork

--- test.php
+++ test.php
@@ -3,26 +3,13 @@
 var_dump($_COOKIE);
 
 
-$counter = 0;
-if(isset($_COOKIE['visits'])){
-    $counter = $_COOKIE['visits'];
-} else {
-    $_COOKIE['visits'] = '';
-}
-$counter++;
+$user_id = $_COOKIE['user_id'] ?? uniqid();
+$visits = ($_COOKIE['visits'] ?? 0) +1;
 
-setcookie('visits', $counter, time() +3600, '/');
-
-$user_id = rand(0, 1000000);
-
-if(isset($_COOKIE['user_id'])) {
-    $user_id = $_COOKIE['user_id'];
-} else {
-    $_COOKIE['user_id'] = '';
-    setcookie('user_id', $user_id, time() + 3600, '/');
-}
+setcookie('visits', $visits, time() +3600, '/');
+setcookie('user_id', $user_id, time() + 3600, '/');
 
 ?>
 
-<h1> <?php print $_COOKIE['user_id']; ?> </h1>
-<h2> <?php print $_COOKIE['visits']; ?> </h2>
+<h1> <?php print $user_id; ?> </h1>
+<h2> <?php print $visits; ?> </h2>
